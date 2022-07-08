dd05a56cf581506c068aa70a1514f2d5e9995e51
Gustavo Saez <github@podapps.net>
2022-07-08 15:00:55 -0300

    Update promoapps.py

--- promoapps.py
+++ promoapps.py
@@ -18,7 +18,7 @@ lastUpdates = 'history'
 user_agent = {'User-agent': 'Mozilla/5.1'}
 
 def send_photo(app_img, app_name, app_link, app_price, app_desc):
-    caption = f'<b>{app_name}</b>\n{app_desc}\n\n💲 {app_price}\n\n⬇️<b>Download: </b>\n {app_link}\n\n---\n@PromoApps'
+    caption = f'<b>.:: {app_name} ::.</b>\n{app_desc}\n\n💲 {app_price}\n\n⬇️ <b>Download: </b>\n {app_link}'
     response = requests.get(app_img)
     open('img.png', 'wb').write(response.content)
     app_img = open('img.png', 'rb')
