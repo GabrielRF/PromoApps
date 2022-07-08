2ec0951e9ec5d5cd21d735152e924948d43db5b3
Gustavo Saez <github@podapps.net>
2022-07-08 11:04:18 -0300

    Update promoapps.py

--- promoapps.py
+++ promoapps.py
@@ -17,11 +17,13 @@ bot = telebot.TeleBot(TOKEN)
 lastUpdates = 'history'
 user_agent = {'User-agent': 'Mozilla/5.1'}
 
-def send_message(app_name, app_link, app_price, app_desc, app_img, btn_link):
-    message = ('<b> ' + str(app_name) + '</b><a href="' + str(app_img) + '">.</a>'
+def send_photo(app_name, app_link, app_price, app_desc, app_img, btn_link):
+    message = (
+        '<b> ' + str(app_name) + '</b>
+        <a href="' + str(app_img) + '">.</a>'
         + '\n' + str(app_desc.replace("'",'')) 
         + '\n\n' + str(app_price))
-    bot.send_message(f'@{DESTINATION}', message, parse_mode='HTML', reply_markup=btn_link)
+    bot.send_photo(f'@{DESTINATION}', message, parse_mode='HTML', reply_markup=btn_link)
 
 def get_site():
     response = requests.get(f'https://t.me/s/{DESTINATION}')
